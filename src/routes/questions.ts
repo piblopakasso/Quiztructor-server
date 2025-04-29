ce8b2e9fee1f9498e26b84c1b8292fe85082e89a
piblopakasso <puccini.ua@gmail.com>
2025-04-29 18:10:39 +0300

    refactor(questions): added questions type and updated path

--- src/routes/questions.ts
+++ src/routes/questions.ts
@@ -1,12 +1,25 @@
 import { Router } from 'express';
-
+import path from 'path'
 import {readJSONFile} from "../utils/readJSONFile";
 
 const router = Router();
 
+const questionsFilePath = path.join(process.cwd(), 'data', 'questions.json')
+
+type Question = {
+    id: number
+    mainText: string
+    additionalText?: string
+    topics: string[]
+    used: boolean
+    reused: boolean
+    mechanics: string[]
+    quizzes?: string[]
+}
+
 router.get('/', async (_req, res) => {
     try {
-        const data = await readJSONFile('./data/questions.json');
+        const data = await readJSONFile<Question[]>(questionsFilePath);
         res.json(data);
     } catch (error) {
         res.status(500).json({ error: 'Помилка читання файлу запитань' });
